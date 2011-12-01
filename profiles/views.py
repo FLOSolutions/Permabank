bd5bd7c92413a7caf4f27efad4095d87ec2d1c4b
Dan Hoizner <dan.hoizner@gmail.com>
2011-12-01 17:28:23 -0500

    profile page (active gifts/wishes)

--- profiles/views.py
+++ profiles/views.py
@@ -20,8 +20,14 @@ class ProfileDetailView(ProfileViewMixin, DetailView):
     template_name = "profiles/profile.html"
     def get_context_data(self, **kwargs):
         context = super(ProfileDetailView, self).get_context_data(**kwargs)
-        context['gifts'] = Gift.objects.order_by('-created', 'title').filter(user__pk=self.object.pk)[:5]
-        context['wishes'] = Wish.objects.order_by('-created', 'title').filter(user__pk=self.object.pk)[:5]
+        #context['gifts'] = Gift.objects.order_by('-created', 'title').filter(user__pk=self.object.pk)[:5]
+        #context['wishes'] = Wish.objects.order_by('-created', 'title').filter(user__pk=self.object.pk)[:5]
+        list1 = list(Gift.objects.order_by('-created', 'title').filter(user__pk=self.object.pk)[:5])
+        list2 = list(Wish.objects.order_by('-created', 'title').filter(user__pk=self.object.pk)[:5])
+        list3 = list1 + list2
+        context['giftswishes'] = sorted(list3, key=lambda record: record.pk, reverse=True)
+        
+            
         return context
 
 @requires_login
