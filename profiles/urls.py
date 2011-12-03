a3a73a321076fc318fee5d9a2212eba8c51aeec0
Dan Hoizner <dan.hoizner@gmail.com>
2011-12-02 23:07:02 -0500

    converted ProfileRecordsView from function view to generic class-based view

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,7 +3,8 @@ from profiles.views import *
 
 urlpatterns = patterns('profiles.views',
     url(r'^$', ProfileListView.as_view()),
-    url(r'^(?P<profile_id>[0-9]+)/records', profile_records),
+    #url(r'^(?P<profile_id>[0-9]+)/records', profile_records),
+    url(r'^(?P<profile_id>[0-9]+)/records', ProfileRecordsView.as_view()),
     url(r'^(?P<pk>[0-9]+)$', ProfileDetailView.as_view(), name='profile'),
     url(r'^edit/$', UserUpdateView.as_view()),
 )
